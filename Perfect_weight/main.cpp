d72244eaf5fe4c41f9c331f9a251ac82d8f9b948
ElisVassiltsova <elis.vassiltsova@gmail.com>
2018-09-13 15:11:39 +0300

    Second commit

--- Perfect_weight/main.cpp
+++ Perfect_weight/main.cpp
@@ -1,8 +1,48 @@
-#include <QCoreApplication>
+#include "iostream"
+#include "string"
 
-int main(int argc, char *argv[])
+#define TOLERANTS 0.10
+using namespace std;
+
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    signed short int idealWeight;
+    char gender;
+    unsigned short int height;
+    float realWeight;
 
-    return a.exec();
+    cout << "Are you male or female? Type M or F:" << endl;
+    cin >> gender;
+
+    cout << "Hello user \n" << "What is your heigth in cm? " << endl;
+    cin >> height;
+
+    cout << "What is your weight in kg? " << endl;
+    cin >> realWeight;
+
+    if (gender == 'F') idealWeight = height * height * 0.00222;
+    else idealWeight = height * height * 0.00227;
+
+    cout << "idealWeight value is " << idealWeight << endl;
+
+    float minWeight = idealWeight * (1.0 - TOLERANTS) ;
+    float maxWeight = idealWeight * (1.0 + TOLERANTS );
+
+    cout << "ideal weight shall be {" <<minWeight << " - "<< maxWeight <<"} kg\n";
+
+    if (realWeight < minWeight) {
+        cout << "You're under ideal weight" << endl;
+    }
+    if ( (realWeight > minWeight) && (realWeight < maxWeight )){
+         cout << "You have ideal weight" << endl;
+    }
+    if (realWeight > maxWeight) {
+         cout << "You're above ideal weight" << endl;
+    }
+
+
+
+    return 0;
+
 }
